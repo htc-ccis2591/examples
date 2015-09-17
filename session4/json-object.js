d4ba348e201b055ff5e35de9c9d472a0a1e90764
mbMosman <mbmosman@users.noreply.github.com>
2015-09-16 20:25:22 -0500

    object access example

---
+++ session4/json-object.js
@@ -0,0 +1,17 @@
+var cats = {
+
+    [
+        {
+            name: "Bart",
+            age: 18,
+            weight: 16,
+        },
+        {
+            name: "Bart",
+            age: 18,
+            weight: 16
+        }
+    ]
+}
+
+console.log(cat.name);
